0bd75e6e475999d0583fab13d8d75175dd16466d
vannarith10 <vyra.vannarith27@gamil.com>
2026-03-31 23:53:22 +0700

    removed localhost to use only base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,8 +9,7 @@ import {
   isRefreshTokenExpired,
 } from "./authService";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
